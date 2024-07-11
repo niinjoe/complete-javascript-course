5f23acfa8eeeb63af2bb1138082dd3dc3cafaf47
Joseph Serrano <80960873+niinjoe@users.noreply.github.com>
2024-07-11 05:45:14 -0400

    Update script.js

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,51 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive.')
+
+// Functions
+function logger() {
+    console.log('My name is John');
+}
+
+// Calling, running or invoking funtion
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges){
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5,0);
+console.log(appleJuice);
+
+// Function declaration
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+const age1 = calcAge1(1991);
+
+// Function expression
+const calcAge2 = function (birthYear){
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2)
+
+// Arrow functions
+const calcAge3 = birthYear => 2037 - birthYear;
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years.`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Bob'));
