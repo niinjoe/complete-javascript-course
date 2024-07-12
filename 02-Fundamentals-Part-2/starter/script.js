8eab0c781bc276161c5422d5eea778c1de950c41
Joseph Serrano <80960873+niinjoe@users.noreply.github.com>
2024-07-12 05:36:02 -0400

    Update script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-let hasDriversLicense = false;
+/*let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriversLicense = true;
@@ -48,4 +48,61 @@ const yearsUntilRetirement = (birthYear, firstName) => {
     return `${firstName} retires in ${retirement} years.`;
 }
 
-console.log(yearsUntilRetirement(1991, 'Bob'));
+console.log(yearsUntilRetirement(1991, 'Bob'));*/
+
+// Arrays Section
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ["Michael", "Steven", " Peter"];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+
+const firstName = "Jonas";
+const jonas = [firstName, "Schmedtmann", 2037 - 1991, "teacher", friends];
+console.log(jonas);
+console.log(jonas.length);
+
+// Exercise
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+const years = [1990, 1967, 2002, 2010, 2018];
+console.log(calcAge(years[0]));
+
+// for age in years {
+//     const age[age] = calcAge(years[age])
+// }
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years.length - 1)];
+console.log(ages);
+
+// Add Elements
+friends.push("Joe");
+console.log(friends);
+
+friends.unshift("John");
+console.log(friends);
+
+// Remove elements
+friends.pop(); // Last
+friends.pop();
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf("Steven"));
+
+console.log(friends.includes("Steven"));
+console.log(friends.includes("Bob"));
