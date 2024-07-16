e48571e7c5b7e5412a1d801425284cba2c00e9ff
Joseph Serrano <80960873+niinjoe@users.noreply.github.com>
2024-07-15 21:01:45 -0400

    Update script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -214,7 +214,7 @@ if (mark.calcBMI() > john.calcBMI()) {
   );
 }
 */
-
+/*
 // Loops Section
 // for loops keeps running while condition is TRUE
 for (let rep = 1; rep < 10; rep++) {
@@ -255,3 +255,80 @@ for (let i = 0; i < jonasArray.length; i++) {
   if (typeof jonasArray[i] !== "string") continue;
   console.log(jonasArray[i], typeof jonasArray[i]);
 }
+
+console.log("--- BREAK WITH NUMBER ---");
+for (let i = 0; i < jonasArray.length; i++) {
+  if (typeof jonasArray[i] === "number") break;
+  console.log(jonasArray[i], typeof jonasArray[i]);
+}
+
+
+// Looping backwards and loops in loops
+const jonasArray = [
+  "Jonas",
+  "Schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+];
+
+for (let i = jonasArray.length - 1; i >= 0; i--) {
+  console.log(i, jonasArray[i]);
+}
+
+for (let excercise = 1; excercise < 4; excercise++) {
+  console.log(`-----Starting exercise ${excercise} -----`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Lifting weight repetition ${rep}...`);
+  }
+}
+
+// While loops
+let rep = 1;
+while (rep <= 10) {
+  console.log(`Lifting weight repetition ${rep}...`);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log("Loop is about to end...");
+}
+*/
+
+/*
+CHALLENGE #4
+
+Let's improve Steven's tip calculator even more, this time using loops!
+
+Your tasks:
+
+Create an array called bills containing all 10 test bill values.
+
+Create empty arrays for the tips and the totals (tips and totals)
+
+Use the calcTip function we wrote before (included in the starter code) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+
+
+
+TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86, and 52.
+
+
+
+BONUS:
+
+Write a function calcAverage which takes an array called arr as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it if you feel like it:
+
+First, you will need to add up all values in the array. To do the addition, start by creating a variable sum that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the sum variable. This way, by the end of the loop, you have all values added together.
+
+To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements).
+
+Call the function with the totals array.
+*/
+
+bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+let tips = [];
